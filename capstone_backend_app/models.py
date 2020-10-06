f88a1f883d1be28b5294e32223304fb8dd0988e1
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-10-06 08:16:09 -0600

    Update to model to implement user, SavedAsteroid, and Comments.

--- capstone_backend_app/models.py
+++ capstone_backend_app/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 
 class MyUser(AbstractUser):
-    username = models.EmailField(max_length=254)
+    username = models.EmailField(max_length=254, unique=True)
     display_name = models.CharField(max_length=80, blank=False, null=False)
 
 
@@ -15,3 +15,18 @@ class SavedAsteroid(models.Model):
     favorite = models.BooleanField(default=False)
     date_saved = models.DateTimeField(default=timezone.now)
     note = models.TextField()
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=240)
+    attatched = models.ForeignKey(SavedAsteroid, on_delete=models.CASCADE)
+    up_vote = models.IntegerField(default=0)
+    down_vote = models.IntegerField(default=0)
+    post_date = models.DateTimeField(default=timezone.now)
+
+    @property
+    def rating(self):
+        return self.up_vote - self.down_vote
+
+    def __str__(self):
+        return self.title
