25b800e1c601a11252c892e0e7ea4a4b95978d9a
Christopher Escobedo <chrisescobedo0617@gmail.com>
2020-10-19 10:39:47 -0400

    hid the secrect key using env

--- capstone_backend_server/settings.py
+++ capstone_backend_server/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '192y8#2sjv-&&cc2^1ffwpkwlljqg-lklly1#q)a)0$t+9_fvx'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
